928a0c104f7bb5c34ac5e51000744afa9ddd3e3e
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2024-05-13 15:28:35 +0600

    Update: chat field describe message

--- app/Services/Form/ChatGPTHelper.php
+++ app/Services/Form/ChatGPTHelper.php
@@ -4,8 +4,7 @@ namespace FluentForm\App\Services\Form;
 
 use FluentForm\App\Models\Form;
 use FluentForm\App\Models\FormMeta;
-use FluentForm\Framework\Helpers\ArrayHelper;
-use FluentForm\Framework\Support\Arr;
+use FluentForm\Framework\Helpers\ArrayHelper as Arr;
 
 class ChatGPTHelper extends FormService
 {
@@ -14,15 +13,16 @@ class ChatGPTHelper extends FormService
         $form = $this->generateForm($req);
         $allFields = $this->getDefaultFields();
         $fluentFormFields = [];
-        $fields = $form['fields'];
+        $fields = Arr::get($form, 'fields', []);
         
         foreach ($fields as $field) {
             if ($inputKey = $this->resolveInput($field)) {
                 $fluentFormFields[] = $this->processField($inputKey, $field, $allFields);
             }
         }
-        $fluentFormFields = $this->maybeAddPayments($fluentFormFields,$allFields);
-        $title = $form['title'] ?? '';
+        $fluentFormFields = $this->maybeAddPayments($fluentFormFields, $allFields);
+      
+        $title = Arr::get($form, 'title', '');
         return $this->saveForm($fluentFormFields, $title);
     }
     
@@ -34,24 +34,30 @@ class ChatGPTHelper extends FormService
             throw new \Exception(__('Query is empty!'));
         }
         
-        $additionalQuery = \FluentForm\Framework\Support\Sanitizer::sanitizeTextField(Arr::get($req,
-            'additional_query'));
+        $additionalQuery = \FluentForm\Framework\Support\Sanitizer::sanitizeTextField(Arr::get($req, 'additional_query'));
         
         if ($additionalQuery) {
-            $query .= "\n including questions for information like  " . $additionalQuery;
+            $query .= "\n including questions for information like  " . $additionalQuery . ".";
         }
-        $query .= " \n return as json fluentform format and code only,
-                    don't include text in response, declare inside fields array,if payment key included add a field type with payment key,
-                    use type key as field type and form title as title key
-                    ";
+//        $query .= " \n return as json fluentform format and code only,
+//                    don't include text in response, declare inside fields array,if payment key included add a field type with payment key,
+//                    use type key as field type and form title as title key
+//                    ";
+
+        $query .= "\nField includes 'type', 'name', 'label', 'placeholder', 'required' status, 'options' if has options will be format 'label' 'value' pair.
+        \nIf has field like full name, first name, last name, field key type will be 'name'. If has field like phone, field key type will be 'phone'.
+        \nIf has field like payment, field key type will be 'payment'.
+        \nAdd 'title' key to define the form title.
+        \nReturn the form data in JSON format, adhering to FluentForm's structure. Only include the form fields inside the 'fields' array.";
+
         $args = [
             "role"    => 'system',
             "content" => $startingQuery . $query,
         ];
         
-        $token = ArrayHelper::get(get_option('_fluentform_openai_settings'), 'access_token');
+        $token = Arr::get(get_option('_fluentform_openai_settings'), 'access_token');
         $result = (new \FluentFormPro\classes\Chat\ChatFieldController(wpFluentForm()))->makeRequest($token, $args);
-        $response = trim(ArrayHelper::get($result, 'choices.0.message.content'), '"');
+        $response = trim(Arr::get($result, 'choices.0.message.content'), '"');
         $response = json_decode($response, true);
 
         if (is_wp_error($response) || empty($response['fields'])) {
@@ -80,28 +86,41 @@ class ChatGPTHelper extends FormService
     
     protected function processField($inputKey, $field, $allFields)
     {
-        $matchedField = $this->getElementByType($allFields,$inputKey);
+        $matchedField = $this->getElementByType($allFields, $inputKey);
         $matchedField['uniqElKey'] = "el_" . uniqid();
-        
-        $label = ArrayHelper::get($field, 'label');
-        $required = ArrayHelper::isTrue($field, 'required');
-        $options = ArrayHelper::get($field, 'options');
-        
-        if ($label) {
+
+        if ($label = Arr::get($field, 'label')) {
+            $required = Arr::isTrue($field, 'required');
             if (isset($matchedField['settings']['label'])) {
                 $matchedField['settings']['label'] = $label;
                 if (isset($matchedField['settings']['validation_rules']['required']['value'])) {
                     $matchedField['settings']['validation_rules']['required']['value'] = $required;
                 }
-            } elseif (isset($matchedField['fields'])) {
+            }
+
+            $placeholder = Arr::get($field, 'placeholder');
+            if ($placeholder) {
+                if (isset($matchedField['attributes']['placeholder'])) {
+                    $matchedField['attributes']['placeholder'] = $placeholder;
+                } elseif (isset($matchedField['settings']['placeholder'])) {
+                    $matchedField['settings']['placeholder'] = $placeholder;
+                }
+            }
+
+            if (isset($matchedField['fields'])) {
                 $subFields = $matchedField['fields'];
-                $subNames = explode(" ",$label);
+                $subNames = explode(" ", $label);
                 if (count($subNames) > 1) {
                     $counter = 0;
                     foreach ($subFields as $subFieldkey => $subFieldValue) {
-                        if (isset($subFieldValue['settings']['label']) && ArrayHelper::get($subNames, $counter)) {
-                            $subFields[$subFieldkey]['settings']['label'] = ArrayHelper::get($subNames, $counter);
-                            $subFields[$subFieldkey]['settings']['validation_rules']['required']['value'] = $required;
+                        if (Arr::get($subNames, $counter)) {
+                            if (Arr::has($subFieldValue, 'settings.visible') && !Arr::isTrue($subFieldValue, 'settings.visible')) {
+                                continue;
+                            }
+                            if (isset($subFieldValue['settings']['label'])) {
+                                $subFields[$subFieldkey]['settings']['label'] = Arr::get($subNames, $counter);
+                                $subFields[$subFieldkey]['settings']['validation_rules']['required']['value'] = $required;
+                            }
                             $counter++;
                         }
                     }
@@ -110,8 +129,7 @@ class ChatGPTHelper extends FormService
             }
         }
         
-        if ($options) {
-            $options = $this->getOptions($options);
+        if ($options = $this->getOptions(Arr::get($field, 'options'))) {
             if (isset($matchedField['settings']['advanced_options'])) {
                 $matchedField['settings']['advanced_options'] = $options;
             }
@@ -141,14 +159,26 @@ class ChatGPTHelper extends FormService
         if (empty($options) || !is_array($options)) {
             return $options;
         }
-        foreach ($options as $key => $value) {
-            $label = $value['label'] ??$value;
-            $key = $value['value'] ??$key;
-            $arr = [
+        foreach ($options as $key => $option) {
+            if (is_string($option) || is_numeric($option)) {
+                $value = $label = $option;
+            } elseif (is_array($option)) {
+                $label = Arr::get($option, 'label');
+                $value = Arr::get($option, 'value');
+            } else {
+                continue;
+            }
+            if (!$value || !$label) {
+                $value = $value ?? $label;
+                $label = $label ?? $value;
+            }
+            if (!$value || !$label) {
+                continue;
+            }
+            $formattedOptions[] = [
                 'label' => $label,
-                'value' => $key,
+                'value' => $value,
             ];
-            $formattedOptions[] = $arr;
         }
         
         return $formattedOptions;
@@ -183,29 +213,28 @@ class ChatGPTHelper extends FormService
         do_action('fluentform/inserted_new_form', $form->id, $data);
         return $form;
     }
-    
-    protected function maybeAddPayments($fluentFormFields,$allFields)
+
+    protected function maybeAddPayments($fluentFormFields, $allFields)
     {
-        $paymentElements = ['payment_method','multi_payment_component'];
+        $paymentElements = ['payment_method', 'multi_payment_component'];
         $foundElements = [];
-        if(empty($fluentFormFields)){
-            return  [];
+        if (empty($fluentFormFields)) {
+            return [];
         }
         foreach ($fluentFormFields as $item) {
-            if ( in_array($item["element"],$paymentElements)) {
+            if (in_array($item["element"], $paymentElements)) {
                 $foundElements[] = $item["element"];
             }
         }
         // Find the elements in $paymentElements that are not in $foundElements
         $remainingElements = array_diff($paymentElements, $foundElements);
         $formPaymentElm = [];
-        if(!empty($remainingElements)){
-            foreach ($remainingElements as $elmKey){
-                $formPaymentElm[] = $this->getElementByType($allFields,$elmKey);
+        if ($foundElements && !empty($remainingElements)) {
+            foreach ($remainingElements as $elmKey) {
+                $formPaymentElm[] = $this->getElementByType($allFields, $elmKey);
             }
         }
-      
-        return array_merge($fluentFormFields,$formPaymentElm);
+        return array_merge($fluentFormFields, $formPaymentElm);
     }
     
 }
